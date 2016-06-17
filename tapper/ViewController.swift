6eb2fa56dbd8b7bfb29eff8b32cf9441bf98f692
Daniel Soares <danielsoares@macbook-pro-de-daniel.local>
2016-06-16 22:56:03 -0400

    First Commit.

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var img1: UIImageView!
+    
+    @IBOutlet weak var btnStart: UIButton!
+    
+    @IBOutlet weak var texto: UITextField!
+    
+    @IBOutlet weak var btnAcao: UIButton!
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    var maxTaps = 0;
+    var currentTaps = 0;
+    
+    @IBAction func start(sender: UIButton) {
+        
+        if (texto.text != nil && texto.text != ""){
+            btnAcao.hidden = false
+            lbl.hidden = false
+            img1.hidden = true
+            btnStart.hidden = true
+            texto.hidden = true
+            
+            maxTaps = Int(texto.text!)!
+            currentTaps = 0;
+            lbl.text = "\(currentTaps)"
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func clicar(sender: AnyObject) {
+        currentTaps = currentTaps + 1;
+        lbl.text = "\(currentTaps)"
+        
+        
+        if (currentTaps == maxTaps){
+            btnAcao.hidden = true
+            lbl.hidden = true
+            img1.hidden = false
+            btnStart.hidden = false
+            texto.hidden = false
+            
+            maxTaps = 0;
+            currentTaps = 0;
+            
+            texto.text = ""
+            lbl.text = ""
+        }
+        
     }
-
-
+    
+    
 }
 
